d8702a01772270c601ab491d19a24c5647d9df82
Edward <114746190+Ed-cred@users.noreply.github.com>
2024-01-08 11:57:14 +0200

    Day2 (#1)
    * Fixed return bug in function
    
    * Day2 solution and trial test

--- day1_ajs.js
+++ day1_ajs.js
@@ -2,12 +2,9 @@ const firstDupe = (gifts) => {
     const dict = {};
     for(const [i,v] of gifts.entries()) {
         if(dict.hasOwnProperty(v)){
-            return dict[v];
+            return v;
         }
         dict[v] = i;
-
     }
     return -1;
 };
-arr = [1, 3, 5, 1, 3];
-console.log(firstDupe(arr));
