a7cbb74dba10a6f19724a7dbf2c46392acd7281f
Ed-cred <edwardandrew63@gmail.com>
2024-01-09 17:09:31 +0200

    parsing setup

--- day8_ajs.js
+++ day8_ajs.js
@@ -1,12 +1,19 @@
 function organizeGifts(gifts) {
+    let result = "";
     const sepGifts = []
-    const start = 0;
+    let start = 0;
     for (let i = 0; i < gifts.length; ++i) {
-        if (gifts[i].toLowerCase() !== gift[i].toUpperCase()) {
-           sepGifts.push(gifts.substring(start, i));
+        if (gifts[i].toLowerCase() !== gifts[i].toUpperCase()) {
+           sepGifts.push(gifts.substring(start, i + 1));
            start = i + 1;
         }
     }
+    // for (const giftType of sepGifts) {
+    //     let arr = "";
+    //     while (giftType % 10 === 0) {
+    //     }
+    // }
+    console.log(sepGifts);
 }
 
 const gifts = '76a11b';
