2ed76c0b2055e9a598e60421dcf029df8cde4f4d
Ed-cred <edwardandrew63@gmail.com>
2024-01-18 11:02:11 +0200

    factored out recursive function after fighting the undefined checks

--- day16_ajs.js
+++ day16_ajs.js
@@ -1,41 +1,25 @@
-function transformTree(tree) {
-    if (tree[0] === null) {
+function traverse(tree, index) {
+  if (!tree || index >= tree.length || (tree[index] === null) | undefined) {
     return null;
   }
 
-  const traverse = (index) => {
-    if (tree[index] === null || index >= tree.length) {
-      return null;
-    }
-
-    const lindex = 2 * index + 1;
-    const rindex = 2 * index + 2;
-    return {
-      value: tree[index],
-      left: traverse([lindex]),
-      right: traverse([rindex]),
-    };
+  const lindex = 2 * index + 1;
+  const rindex = 2 * index + 2;
+  return {
+    value: tree[index],
+    left: traverse(tree, lindex),
+    right: traverse(tree, rindex),
   };
-  const result = traverse(0);
+}
+
+function transformTree(tree) {
+  if (!tree || (tree[0] === null) | undefined) {
+    return null;
+  }
+  const result = traverse(tree, 0);
   return result;
 }
 
-// function traverse(index, tree) {
-//   if ((tree && tree[index] === null) || (tree && index >= tree.length)) {
-//     return null;
-//   }
-//   //left and right child index based on provided binary tree array representation
-//   const lindex = 2 * index + 1;
-//   const rindex = 2 * index + 2;
-//   // return the current node with recursive calls for the left and right children to traverse the tree
-//   if (tree) {
-//     return {
-//       value: tree[index],
-//       left: traverse([lindex]),
-//       right: traverse([rindex]),
-//     };
-//   }
-// }
 console.log(transformTree([3, 1, 0, 8, 12, null, 1]));
 /*
 {
