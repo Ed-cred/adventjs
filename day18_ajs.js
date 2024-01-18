2073da5501058328596d53b4a493e3228c17b8e0
Ed-cred <edwardandrew63@gmail.com>
2024-01-18 16:25:52 +0200

    tried for a solution, didn't work, need to take a second look, i hate drawing

---
+++ day18_ajs.js
@@ -0,0 +1,49 @@
+function drawClock(time) {
+    const digitMappings = [
+      ['***', '* *', '* *', '* *', '***'],  // 0
+      [' * ', '** ', ' * ', ' * ', '***'],  // 1
+      ['***', '  *', '***', '*  ', '***'],  // 2
+      ['***', '  *', '***', '  *', '***'],  // 3
+      ['* *', '* *', '***', '  *', '  *'],  // 4
+      ['***', '*  ', '***', '  *', '***'],  // 5
+      ['***', '*  ', '***', '* *', '***'],  // 6
+      ['***', '  *', '  *', '  *', '  *'],  // 7
+      ['***', '* *', '***', '* *', '***'],  // 8
+      ['***', '* *', '***', '  *', '***'],  // 9
+    ];
+  
+    const padAndSplitDigits = number => String(number).padStart(2, '0').split('').map(Number);
+    // Construct the clock array directly
+    const clock = Array.from({ length: 7 }, () => Array(17).fill(' '));
+    const [hours, minutes] = time.split(':').map(Number);
+  
+    const placeDigits = (digits, startColumn) => {
+      digits.forEach((digit, digitIndex) => {
+        digitMappings[digit].forEach((row, rowIndex) => {
+          clock[rowIndex][startColumn + digitIndex * 4] = row[0];
+          clock[rowIndex][startColumn + digitIndex * 4 + 1] = row[1];
+          clock[rowIndex][startColumn + digitIndex * 4 + 2] = row[2];
+        });
+      });
+    };
+  
+    placeDigits(padAndSplitDigits(hours), 0);
+  
+    clock[1][8] = clock[3][8] = '*';
+  
+    placeDigits(padAndSplitDigits(minutes), 10);
+  
+    return clock.map(row => row.join(''));
+}
+console.log(drawClock("01:30")); // ️
+/*
+[
+  ['*', '*', '*', ' ', ' ', ' ', '*', ' ', ' ', ' ', '*', '*', '*', ' ', '*', '*', '*'],
+  ['*', ' ', '*', ' ', ' ', ' ', '*', ' ', ' ', ' ', ' ', ' ', '*', ' ', '*', ' ', '*'],
+  ['*', ' ', '*', ' ', ' ', ' ', '*', ' ', '*', ' ', ' ', ' ', '*', ' ', '*', ' ', '*'],
+  ['*', ' ', '*', ' ', ' ', ' ', '*', ' ', ' ', ' ', '*', '*', '*', ' ', '*', ' ', '*'],
+  ['*', ' ', '*', ' ', ' ', ' ', '*', ' ', '*', ' ', ' ', ' ', '*', ' ', '*', ' ', '*'],
+  ['*', ' ', '*', ' ', ' ', ' ', '*', ' ', ' ', ' ', ' ', ' ', '*', ' ', '*', ' ', '*'],
+  ['*', '*', '*', ' ', ' ', ' ', '*', ' ', ' ', ' ', '*', '*', '*', ' ', '*', '*', '*']
+]
+*/
