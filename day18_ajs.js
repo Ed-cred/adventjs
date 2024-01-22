7f2b9e73dbac6798ffac92d7b83509adf37d1d47
Ed-cred <edwardandrew63@gmail.com>
2024-01-22 08:46:27 +0200

    Day 18 solution, some perfromance overhead with spread, but otherwise pretty nice imo

--- day18_ajs.js
+++ day18_ajs.js
@@ -1,39 +1,29 @@
 function drawClock(time) {
-    const digitMappings = [
-      ['***', '* *', '* *', '* *', '***'],  // 0
-      [' * ', '** ', ' * ', ' * ', '***'],  // 1
-      ['***', '  *', '***', '*  ', '***'],  // 2
-      ['***', '  *', '***', '  *', '***'],  // 3
-      ['* *', '* *', '***', '  *', '  *'],  // 4
-      ['***', '*  ', '***', '  *', '***'],  // 5
-      ['***', '*  ', '***', '* *', '***'],  // 6
-      ['***', '  *', '  *', '  *', '  *'],  // 7
-      ['***', '* *', '***', '* *', '***'],  // 8
-      ['***', '* *', '***', '  *', '***'],  // 9
-    ];
-  
-    const padAndSplitDigits = number => String(number).padStart(2, '0').split('').map(Number);
-    // Construct the clock array directly
-    const clock = Array.from({ length: 7 }, () => Array(17).fill(' '));
-    const [hours, minutes] = time.split(':').map(Number);
-  
-    const placeDigits = (digits, startColumn) => {
-      digits.forEach((digit, digitIndex) => {
-        digitMappings[digit].forEach((row, rowIndex) => {
-          clock[rowIndex][startColumn + digitIndex * 4] = row[0];
-          clock[rowIndex][startColumn + digitIndex * 4 + 1] = row[1];
-          clock[rowIndex][startColumn + digitIndex * 4 + 2] = row[2];
-        });
-      });
-    };
-  
-    placeDigits(padAndSplitDigits(hours), 0);
-  
-    clock[1][8] = clock[3][8] = '*';
-  
-    placeDigits(padAndSplitDigits(minutes), 10);
-  
-    return clock;
+  const digitMapping = {
+    0: ["***", "* *", "* *", "* *", "* *", "* *", "***"],
+    1: ["  *", "  *", "  *", "  *", "  *", "  *", "  *"],
+    2: ["***", "  *", "  *", "***", "*  ", "*  ", "***"],
+    3: ["***", "  *", "  *", "***", "  *", "  *", "***"],
+    4: ["* *", "* *", "* *", "***", "  *", "  *", "  *"],
+    5: ["***", "*  ", "*  ", "***", "  *", "  *", "***"],
+    6: ["***", "*  ", "*  ", "***", "* *", "* *", "***"],
+    7: ["***", "  *", "  *", "  *", "  *", "  *", "  *"],
+    8: ["***", "* *", "* *", "***", "* *", "* *", "***"],
+    9: ["***", "* *", "* *", "***", "  *", "  *", "***"],
+    ":": [" ", " ", "*", " ", "*", " ", " "],
+  };
+  const res = digitMapping[time[0]].map((row, pos) => [
+    ...row,
+    " ",
+    ...digitMapping[time[1]][pos],
+    " ",
+    ...digitMapping[":"][pos],
+    " ",
+    ...digitMapping[time[3]][pos],
+    " ",
+    ...digitMapping[time[4]][pos]
+  ]);
+  return res;
 }
 console.log(drawClock("01:30")); // ️
 /*
