a9b6ef4266c7dc7851544fb1ba864f5ecda66663
Ed-cred <edwardandrew63@gmail.com>
2024-01-22 09:44:00 +0200

    Day 19 solution, computing the sum for every elem might not be optimal

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,20 @@
+function revealSabotage(store) {
+    return [];
+}
+
+const store = [
+  ['*', ' ', ' ', ' '],
+  [' ', ' ', '*', ' '],
+  [' ', ' ', ' ', ' '],
+  ['*', ' ', ' ', ' ']
+]
+
+console.log(revealSabotage(store))
+/* Should display:
+[
+    ['*', '2', '1', '1'],
+    ['1', '2', '*', '1'],
+    ['1', '2', '1', '1'],
+    ['*', '1', ' ', ' ']
+]
+*/
